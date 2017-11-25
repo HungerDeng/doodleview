0f70230ebcad6ae66bced18780cde0aee9d2000c
dengkmfromCG <1530643045@qq.com>
2017-11-25 23:03:11 +0800

    sss

--- skinlibrary/src/main/java/com/example/skinlibrary/attr/base/DynamicAttr.kt
+++
@@ -1,6 +0,0 @@
-package com.example.skinlibrary.attr.base
-
-/**
- * Created by MBENBEN on 2017/9/3.
- */
-data class DynamicAttr(val attrName:String,val refResId:Int)
--- skinlibrary/src/main/java/com/example/skinlibrary/listener/ISkinUpdate.kt
+++
@@ -1,8 +0,0 @@
-package com.example.skinlibrary.listener
-
-/**
- * Created by MBENBEN on 2017/9/2.
- */
-interface ISkinUpdate {
-    fun onThemeUpdate()
-}
